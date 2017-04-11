cdf391a6b49e9f2e79ebba25b507a86d5295aebd
bihariel <bihariel@gmail.com>
2017-04-11 15:34:07 +0200

    Update Android.mk

--- configs/Android.mk
+++ configs/Android.mk
@@ -27,18 +27,18 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/data
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := qdcm_calib_data_le_x2_mdss_dsi_sharp_qhd_dualdsi_cmd.xml
+LOCAL_MODULE       := qdcm_calib_data_le_x910_mdss_dsi_sharp_qhd_dualdsi_cmd.xml
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := qdcm/qdcm_calib_data_le_x2_mdss_dsi_sharp_qhd_dualdsi_cmd.xml
+LOCAL_SRC_FILES    := qdcm/qdcm_calib_data_le_x910_mdss_dsi_sharp_qhd_dualdsi_cmd.xml
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := qdcm_calib_data_le_x2_mdss_dsi_truly_qhd_dualdsi_cmd_pvt.xml
+LOCAL_MODULE       := qdcm_calib_data_le_x910_mdss_dsi_truly_qhd_dualdsi_cmd_pvt.xml
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := qdcm/qdcm_calib_data_le_x2_mdss_dsi_truly_qhd_dualdsi_cmd_pvt.xml
+LOCAL_SRC_FILES    := qdcm/qdcm_calib_data_le_x910_mdss_dsi_truly_qhd_dualdsi_cmd_pvt.xml
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
 
